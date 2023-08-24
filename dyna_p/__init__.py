8166b2d39bd1fa68935cb86f6fb96c317f1bb37f
antonyhan(韩雪晨) <antonyhan@tencent.com>
2023-08-24 21:31:01 +0800

    dp 动态规划

---
+++ dyna_p/__init__.py
@@ -0,0 +1,7 @@
+"""
+Dynamic programming，简称DP，相关问题
+"""
+
+from .knapsack_pro import *
+
+__all__ = ['knapsack_pro', 'KnapsackPro']
---
+++ dyna_p/kp_te.py
@@ -0,0 +1,14 @@
+
+from dyna_p import KnapsackPro, knapsack_pro
+
+kp = KnapsackPro([5, 3, 2, 1, 3], [1, 2, 3, 4, 5])
+self = kp
+# self.solve_01(5)
+# self.solve_01(6)
+# self.solve_01(6)
+# self.solve_01(19)
+
+self.solve_0_inf(5)
+self.solve_0_inf(6)
+self.solve_0_inf(6)
+self.solve_0_inf(19)
